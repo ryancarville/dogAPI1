d7ac7e77db126afe02961ff16c33fbb82c6ab309
Ryan Carville <ryancarville@gmail.com>
2019-02-13 20:20:44 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+function getDogImages(imgNum){
+    if (imgNum > 50){
+        alert('You entered more than 50. Please enter a valid number.');
+    }
+    for(let i = 0; i < imgNum; i++){
+        if(imgNum <= 50){
+            fetch("https://dog.ceo/api/breeds/image/random")
+                .then(response => response.json())
+                .then(responseJson =>
+                    displayDogs(responseJson))
+                .catch(error => alert('No images could be displayed due to a error.  Please try again later'))
+        }
+    };
+}
+
+function displayDogs(responseJson){
+    console.log(responseJson);
+}
+
+function dogSearch(){
+    $('form').submit(event => {
+        event.preventDefault();
+        let numOfDogs = document.getElementById("numOfDogs").value;
+        getDogImages(numOfDogs);
+    });
+}
+
+$(function() {
+    console.log('Search is loaded');
+    dogSearch();
+});
